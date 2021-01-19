0390a7db3ba0a543bb3daeb5e3db0bfe4789ef58
Rik Kraan <rik.kraan@vantage-ai.com>
2021-01-19 11:45:32 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Deploying a Flask API and MySQL server on Kubernetes
+
+This repo contains code that 
+1) Deploys a MySQL server on a Kubernetes cluster
+2) Attaches a persistent volume to it, so the data remains contained if pods are restarting
+3) Deploys a Flask API to add, delete and modify users in the MySQL database
+
+## Prerequisites
+1. Have `Docker` and the `Kubernetes CLI` (`kubectl`) installed together with `Minikube` (https://kubernetes.io/docs/tasks/tools/)
+
+## Getting started
+1. Clone the repository
+2. Configure `Docker` to use the `Docker daemon` in your kubernetes cluster via your terminal: `eval $(minikube docker-env)`
+3. Pull the latest mysql image from `Dockerhub`: `Docker pull mysql`
+4. Build a kubernetes-api image with the Dockerfile in this repo: `Docker build . -t kubernetes-api`
+
+## Secrets
+`Kubernetes Secrets` can store and manage sensitive information. For this example we will define a password for the
+`root` user of the `MySQL` server using the `Opaque` secret type. For more info: https://kubernetes.io/docs/concepts/configuration/secret/
+
+1. Encode your password in your terminal: `echo -n super-secret-passwod | base64`
+2. Add the output to the `flakapi-secrets.yml` file at the `db_root_password` field
+
+## Deployments
+Get the secrets, persistent volume in place and apply the deployments for the `MySQL` database and `Flask API`
+
+1. Add the secrets to your `kubernetes cluster`: `kubectl apply -f flaskapi-secrets.yml`
+2. Create the `persistent volume` and `persistent volume claim` for the database: `kubectl apply -f mysql-pv.yml`
+3. Create the `MySQL` deployment: `kubectl apply -f mysql-deployment.yml`
+4. Create the `Flask API` deployment: `kubectl apply -f flaskapp-deployment.yml`
+
+You can check the status of the pods, services and deployments.
+
+## Creating database and schema
+The API can only be used if the proper database and schemas are set. This can be done via the terminal.
+1. Get the IP of your `MySQL service`: `kubectl get services | grep mysql | awk '{print $3}'`
+2. Connect to your `MySQL database`: `mysql -u root -host <mysql service IP> -port 3306 -p`
+3. Enter the password specified in the `flaskapi-secrets.yml`
+4. Create the database and table
+   1. `CREATE DATABASE FLASKAPI;`
+    2. `USE FLASKAPI;`
+    3. `CREATE TABLE USERS(user_id INT PRIMARY KEY AUTO_INCREMENT, user_name VARCHAR(255), user_email VARCHAR(255), user_password VARCHAR(255));`
+    
+## Expose the API
+The API can be accessed by exposing it using minikube: `minikube service flask-service`. This will return a `URL`. If you paste this to your browser you will see the `hello world` message. You can use this `service_URL` to make requests to the `API`
+
+## Start making requests
+Now you can use the `API` to `CRUD` your database
+1. add a user: `curl -H "Content-Type: application/json" -d '{"name": "<user_name>", "email": "<user_email>", "pwd": "<user_password>"}' <service_URL>/add`
+2. get all users: `curl <service_URL>/users`
+3. get information of a specific user: `curl <service_URL>/user/<user_id>`
+4. delete a user by user_id: `curl -H "Content-Type: application/json" -d '{"name": "<user_name>", "email": "<user_email>", "pwd": "<user_password>"}' <service_URL>/delete`
+5. update a user's information: `curl -H "Content-Type: application/json" -d {"name": "<user_name>", "email": "<user_email>", "pwd": "<user_password>", "user_id": <user_id>} <service_URL>/update`
---
+++ flaskapi.py
@@ -0,0 +1,135 @@
+"""Code for a flask API to Create, Read, Update, Delete users"""
+import os
+from flask import jsonify, request, Flask
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+
+mysql = MySQL()
+
+# MySQL configurations
+app.config["MYSQL_DATABASE_USER"] = "root"
+app.config["MYSQL_DATABASE_PASSWORD"] = os.getenv("db_root_password")
+app.config["MYSQL_DATABASE_DB"] = os.getenv("db_name")
+app.config["MYSQL_DATABASE_HOST"] = os.getenv("MYSQL_SERVICE_HOST")
+app.config["MYSQL_DATABASE_PORT"] = int(os.getenv("MYSQL_SERVICE_PORT"))
+mysql.init_app(app)
+
+
+@app.route("/")
+def index():
+    """Function to test the functionality of the API"""
+    return "Hello, world!"
+
+
+@app.route("/add", methods=["POST"])
+def add_user():
+    """Function to add a user to the MySQL database"""
+    json = request.json
+    name = json["name"]
+    email = json["email"]
+    pwd = json["pwd"]
+    if name and email and pwd and request.method == "POST":
+        sql = "INSERT INTO users(user_name, user_email, user_password) " \
+              "VALUES(%s, %s, %s)"
+        data = (name, email, pwd)
+        try:
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.execute(sql, data)
+            conn.commit()
+            cursor.close()
+            conn.close()
+            resp = jsonify("User added successfully!")
+            resp.status_code = 200
+            return resp
+        except Exception as exception:
+            return jsonify(str(exception))
+    else:
+        return jsonify("Not provided the right information "
+                       "or used the wrong method")
+
+
+@app.route("/users", methods=["GET"])
+def users():
+    """Function to retrieve all users from the MySQL database"""
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users")
+        rows = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        resp = jsonify(rows)
+        resp.status_code = 200
+        return resp
+    except Exception as exception:
+        return jsonify(str(exception))
+
+
+@app.route("/user/<int:user_id>", methods=["GET"])
+def user(user_id):
+    """Function to get information of a specific user in the MSQL database"""
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE user_id=%s", user_id)
+        row = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        resp = jsonify(row)
+        resp.status_code = 200
+        return resp
+    except Exception as exception:
+        return jsonify(str(exception))
+
+
+@app.route("/update", methods=["POST"])
+def update_user():
+    """Function to update a user in the MYSQL database"""
+    json = request.json
+    name = json["name"]
+    email = json["email"]
+    pwd = json["pwd"]
+    user_id = json["user_id"]
+    if name and email and pwd and user_id and request.method == "POST":
+        # save edits
+        sql = "UPDATE users SET user_name=%s, user_email=%s, " \
+              "user_password=%s WHERE user_id=%s"
+        data = (name, email, pwd, user_id)
+        try:
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.execute(sql, data)
+            conn.commit()
+            resp = jsonify("User updated successfully!")
+            resp.status_code = 200
+            cursor.close()
+            conn.close()
+            return resp
+        except Exception as exception:
+            return jsonify(str(exception))
+    else:
+        return jsonify("Not provided the right information or "
+                       "used the wrong method")
+
+
+@app.route("/delete/<int:user_id>")
+def delete_user(user_id):
+    """Function to delete a user from the MySQL database"""
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM users WHERE user_id=%s", user_id)
+        conn.commit()
+        cursor.close()
+        conn.close()
+        resp = jsonify("User deleted successfully!")
+        resp.status_code = 200
+        return resp
+    except Exception as exception:
+        return jsonify(str(exception))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
